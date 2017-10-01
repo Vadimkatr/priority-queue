e204364a0e224c97b276c1756859e72e962b5b2c
Vadimkatr <vadimlisai@gmail.com>
2017-10-02 00:53:21 +0300

    Added some methods to the class PriorityQueue.

--- src/queue.js
+++ src/queue.js
@@ -2,24 +2,32 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-		this.size = maxSize ? maxSize : 30;
-
+		this.maxSize = maxSize ? maxSize : 30;
+		this.heap = new MaxHeap();
+		this.length = 0;
 	}
 
 	push(data, priority) {
-		
+		if(this.length === this.maxSize)
+			throw new Error();
+		this.heap.push(data, priority);
+		this.length++;
 	}
 
 	shift() {
-
+		if(this.length == 0)
+			throw new Error();
+		this.length--;
+		return this.heap.pop();
+		
 	}
 
 	size() {
-
+		return this.length;
 	}
 
 	isEmpty() {
-		
+		return this.length === 0 ? true : false;
 	}
 }
 
